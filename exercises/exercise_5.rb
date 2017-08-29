d6b6a08f9239de047617e07f0e3e37cff8ba114a
Hannah Kim <h.heeeun.kim@gmail.com>
2017-08-29 19:28:10 +0000

    did caluculation regarding annual revene

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@sum = Store.sum("annual_revenue")
+@num = Store.count
+
+puts "Total revenue for entire compnay is #{@sum}"
+puts "Total revenue for entire compnay is #{@sum / @num}"
+
+@oneMil= Store.where("annual_revenue > ?", 1000000).count
+
+puts "#{@oneMil} is the number of stores that is generating $1M or more in annaul sales"
